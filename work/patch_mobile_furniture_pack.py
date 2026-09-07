d1178cff9aaaa76c25623d61e9e11de24381aafa
Lorsieab2 <23109101+Lorsieab2@users.noreply.github.com>
2026-09-07 00:56:27 -0500

    Forward native PlanToGo arguments

--- work/patch_mobile_furniture_pack.py
+++ work/patch_mobile_furniture_pack.py
@@ -33014,9 +33014,13 @@ static void __cdecl VF2PlanToGoAtAddedFurnitureImpl(
 extern "C" __declspec(naked) void VF2PlanToGoAtAddedFurniture()
 {
     __asm {
+        push dword ptr [esp+16]
+        push dword ptr [esp+16]
+        push dword ptr [esp+16]
+        push dword ptr [esp+16]
         push ecx
         call VF2PlanToGoAtAddedFurnitureImpl
-        add esp, 4
+        add esp, 20
         ret 16
     }
 }
@@ -33040,9 +33044,13 @@ static bool __cdecl VF2PlanToGoObjectAtAddedFurnitureImpl(
 extern "C" __declspec(naked) void VF2PlanToGoObjectAtAddedFurniture()
 {
     __asm {
+        push dword ptr [esp+16]
+        push dword ptr [esp+16]
+        push dword ptr [esp+16]
+        push dword ptr [esp+16]
         push ecx
         call VF2PlanToGoObjectAtAddedFurnitureImpl
-        add esp, 4
+        add esp, 20
         ret 16
     }
 }
